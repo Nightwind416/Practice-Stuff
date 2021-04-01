48f939da5e86bb1b28c1afb09873f806997ca633
Nightwind416 <christopher.e.lorr@gmail.com>
2021-04-01 16:36:53 -0500

    Start adding debugger code

---
+++ FizzBuzz/FizzBuzz.js
@@ -0,0 +1,13 @@
+let msg = 'Hello World';
+console.log(msg);
+
+
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0) {
+        console.log("Fizz")
+    } else {
+        console.log(i);
+    }
+}
